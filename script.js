97981d0472a8ed93709991f93f29f5e7a8d10aeb
Александр Осипов <aleosi@yandex-team.ru>
2019-09-05 11:35:17 +0300

    tmp

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById('btn');
+var video = document.getElementById('video');
+
+btn.addEventListener('click', function(e){
+    video.setAttribute('src', 'https://video-preview.s3.yandex.net/sTNfhQAAAAA.mp4');
+});
