1f989698e3bffbb6c7274c7ecea78c1144076ba4
Александр Осипов <aleosi@yandex-team.ru>
2020-02-04 15:27:53 +0300

    wtf

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var btn = document.getElementById('btn');
 var video = document.getElementById('video');
+var video2 = document.getElementById('video2');
+var source = document.getElementById('source');
 
 btn.addEventListener('click', function(e){
     video.setAttribute('src', 'https://video-preview.s3.yandex.net/sTNfhQAAAAA.mp4');
 });
+
+video.addEventListener('loadeddata', function(e){
+    console.log('video');
+    console.log(e);
+});
+
+video2.addEventListener('loadeddata', function(e){
+    console.log('video2');
+    console.log(e);
+});
+
+source.addEventListener('loadeddata', function(e){
+    console.log('source');
+    console.log(e);
+});
+
